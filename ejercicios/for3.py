f36964fccae17eb8f1c824dbec6ed231df5eadbb
jo1331xd <josecarvallo777@gmail.com>
2025-05-08 16:41:17 -0600

    En teoria corregido

--- ejercicios/for3.py
+++ ejercicios/for3.py
@@ -1,11 +1,13 @@
-numero_calificaciones=int(input("Cual es el numero de calificaciones que tienes"))
-for i in range(numero_calificaciones):
-    calificacion=int(input("Cual es la calificacion que tienes: "))
-    if numero_calificaciones == 0:
-        print("Adios")
-        break
+def calificaciones ():
+    numero_calificaciones=int(input("Cual es el numero de calificaciones que tienes"))
+    for i in range(numero_calificaciones):
+        calificacion=int(input("Cual es la calificacion que tienes: "))
+        if numero_calificaciones == 0:
+            print("Adios")
+            break
 
-else:  
-    calificacion *= numero_calificaciones
+        else:  
+            calificacion *= numero_calificaciones
 
-    print(f"Tienes de promedio {calificacion}")
+            print(f"Tienes de promedio {calificacion}")
+calificaciones()
