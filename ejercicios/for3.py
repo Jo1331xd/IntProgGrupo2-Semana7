21216d945a9dfb4dd0e10b6bf4c0880d23a8a692
jo1331xd <josecarvallo777@gmail.com>
2025-05-08 16:35:03 -0600

    Calificaciones

---
+++ ejercicios/for3.py
@@ -0,0 +1,11 @@
+numero_calificaciones=int(input("Cual es el numero de calificaciones que tienes"))
+for i in range(numero_calificaciones):
+    calificacion=int(input("Cual es la calificacion que tienes: "))
+    if numero_calificaciones == 0:
+        print("Adios")
+        break
+
+else:  
+    calificacion *= numero_calificaciones
+
+    print(f"Tienes de promedio {calificacion}")
