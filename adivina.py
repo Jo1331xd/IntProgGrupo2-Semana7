53280ecc0a907eaa6bd4630aba4963c2e2003db4
jo1331xd <josecarvallo777@gmail.com>
2025-05-08 07:13:38 -0600

    Trabajando con el for

---
+++ adivina.py
@@ -0,0 +1,34 @@
+import datetime
+import random
+import time as t 
+import os 
+
+fecha= datetime.date.today()
+
+print(f"Bienvenida \n {fecha}")
+
+def esperar (espera):
+    t.sleep(1)
+    for i in range(espera):
+        os.system("cls || clear ")
+        print(f"Espera {i}")
+    os.system("cls || clear ")
+
+def adivinar (num_user, num_rdn):
+    esperar(5)
+    if num_user == num_rdn:
+        print ("Adivinaste ")
+    else:
+        print("Lo siento, no es el numero ") 
+        
+def main():
+    numero_alea = random.randint (1,10)
+    resp = "s"
+    while resp.lower() != 'n':
+        
+        num= input("Ingresa un numero del 1 al 10:" )
+        adivinar (int(num), numero_alea)
+        resp = input ("Desea seguir jugando? [ S / N / R ]: ")
+        if resp.lower == 'r':
+            num_alea = random.randit (1,10)
+main()
