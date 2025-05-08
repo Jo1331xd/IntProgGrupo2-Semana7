ab80bbeb5dbbb292080a79fd6d192be7418bf0d2
jo1331xd <josecarvallo777@gmail.com>
2025-05-08 16:31:21 -0600

    reparacion

--- ejercicios/for2.py
+++ ejercicios/for2.py
@@ -1,8 +1,8 @@
 def palabra_conteo():
     for _ in range(100000):
         palabra = input("Ingrese una palabra: ")
-        if palabra.lower() == "Terminar":
-            print("adios")
+        if palabra.lower() == "terminar":
+            print("Adiós")
             break
         else:
             print(f"Esta {palabra.capitalize()} tiene {len (palabra)} letras")
